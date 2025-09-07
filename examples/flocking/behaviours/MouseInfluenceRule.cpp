a53afb43f4f90e2890d11ee9eacbe9d94fb6bf6a
Conor Byrne <conor.byrne@mymail.champlain.edu>
2025-09-07 15:20:12 -0400

    worked on mouse influence, but it doesnt work as intended

--- examples/flocking/behaviours/MouseInfluenceRule.cpp
+++ examples/flocking/behaviours/MouseInfluenceRule.cpp
@@ -2,6 +2,7 @@
 #include "../gameobjects/Boid.h"
 #include "../gameobjects/World.h"
 #include "engine/Engine.h"
+#include <iostream>
 
 Vector2f MouseInfluenceRule::computeForce(const std::vector<Boid*>& neighborhood, Boid* boid) {
   ImGui::SetCurrentContext(world->engine->window->imGuiContext);
@@ -21,6 +22,33 @@ Vector2f MouseInfluenceRule::computeForce(const std::vector<Boid*>& neighborhood
   //    }
   //    else
   //        return Vector2f::zero();
+
+  ImGuiIO& io = ImGui::GetIO();
+  if (ImGui::IsMousePosValid() && io.MouseDown[0]) {
+      Vector2f mousePos(io.MousePos.x, io.MousePos.y);
+      Vector2f displacement = boid->getPosition() - mousePos;
+      float distance = displacement.getMagnitude();
+
+      //The force is inversely proportional to distance
+      Vector2f force = Vector2f::zero();
+
+    if (distance > 0) {
+      Vector2f unitVector = displacement / distance;
+      Vector2f force = (unitVector / distance) * weight;
+    }
+
+      if (isRepulsive)
+          force *= -1.f;
+
+      return force;
+  }
+  else {
+    //std::cout<< "no";
+    return Vector2f::zero();
+  }
+
+
+
   return Vector2f::zero();
 }
 
