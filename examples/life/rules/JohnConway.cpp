c9c119d06c1b01bccff9b6b218bbaa5ca0b6dd61
Conor Byrne <conor.byrne@mymail.champlain.edu>
2025-09-14 17:00:06 -0400

    finished game of life

--- examples/life/rules/JohnConway.cpp
+++ examples/life/rules/JohnConway.cpp
@@ -4,46 +4,44 @@
 void JohnConway::Step(World& world) {
   // todo: implement
 
-// formal code
   for (int i = 0; i < world.SideSize(); i++) {
     for (int j = 0; j < world.SideSize(); j++) {
-      //int neighbors = countNeighbors(i, j);
-      int neighbors = CountNeighbors(world, world.Get());
-      bool currentState = cell[i][j];
+      Point2D currentPoint(j, i);
+      int neighbors = CountNeighbors(world, currentPoint);
+      bool currentState = world.Get(currentPoint);
+
       if (currentState) {
-        world.setCurrent(i, j, neighbors == 2 || neighbors == 3);
+        world.SetNext(currentPoint, neighbors == 2 || neighbors == 3);
       } else {
-        set(i, j, neighbors == 3);
+        world.SetNext(currentPoint, neighbors == 3);
       }
     }
   }
-  swapBuffer();
-  //end of formal code
-
+  world.SwapBuffers();
 }
 
 int JohnConway::CountNeighbors(World& world, Point2D point) {
   // todo: implement
 
-int worldSize = world.SideSize();
-
-// formal code
+  int worldSize = world.SideSize();
   int alive = 0;
+
   for (int i = -1; i <= 1; i++) {
     for (int j = -1; j <= 1; j++) {
       if (i == 0 && j == 0) {
         continue;
       }
 
-      int yCoord = (y + i + lines) % lines;
-      int xCoord = (x + j + columns) % columns;
+      int yCoord = (point.y + i + worldSize) % worldSize;
+      int xCoord = (point.x + j + worldSize) % worldSize;
 
-      if (cell[yCoord][xCoord]) {
+      Point2D neighborPoint(xCoord, yCoord);
+
+      if (world.Get(neighborPoint)) {
         alive++;
       }
     }
   }
+
   return alive;
-// end of formal code
-  return 0;
 }
